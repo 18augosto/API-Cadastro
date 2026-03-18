ded7b2ba99757b973e494aa4a733272b620754c6
hemilyrodrigues-ctrl <emilly.candido.souza@escola.pr.gov.br>
2026-03-18 09:18:05 -0300

    feat:cadastro cliente

--- index.js
+++ index.js
@@ -12,11 +12,15 @@ CLIENTES ENDPOINTS
  */
 const clientesPath = path.join(__dirname, 'clientes.json');
 
+/*
+PRODUTOS ENDPOINTS
+ */
+const produtosPath = path.join(__dirname, 'produtos.json');
+
 function lerClientes() {
     if(!fs.existsSync(clientesPath)){
         return [];
     }
-
 
     const dados = fs.readFileSync(clientesPath, 'utf8');
     try{
@@ -50,8 +54,64 @@ app.post('/clientes', (req, res) => {
     res.status(201).json({ message: 'Cliente criado com sucesso', cliente: novoCliente});
 });
 
+app.get('/clientes', (req, res) => {
+    const clientes = lerClientes();
+    res.json(clientes);
+});
+
+app.get('/clientes/:cpf', (req, res) => {
+    const clientes = lerClientes();
+    const cliente = clientes.find(c => c.cpf === req.params.cpf);
+    if (!cliente) {
+        return res.status(404).json({ error: 'Cliente não encontrado' });
+    }
+    res.status(200).json(cliente);
+});
 
     
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
+
+
+
+function lerProdutos() {
+    if(!fs.existsSync(produtosPath)){
+        return [];
+    }
+
+    const dados = fs.readFileSync(produtosPath, 'utf8');
+    try{
+       return JSON.parse(dados)  || [];
+
+    }catch(e){
+       return [];
+    }
+}
+
+function salvarProdutos(produtos) {
+  fs.writeFileSync(produtosPath, JSON.stringify(produtos, null, 2), 'utf-8');
+} 
+
+app.post("/produtos", (req, res) => {
+    const {id, nome, valor, descrição} = req.body;
+    if (!id || !nome || !valor || !descrição) {
+        return res.status(400).json({ error:"Todos os campos são obrigatórios"});
+
+    }
+    const produtos = lerProdutos();
+    if (produtos.some(p => p.id === id)) {
+        return res.status(400).json({ error: 'ID já cadastrado' });
+    }
+    const novoProduto = {
+        id,
+        nome,
+        valor,
+        descrição
+    };
+    produtos.push(novoProduto);
+    salvarProdutos(produtos);
+    res.status(201).json({message: 'Produto cadastrado com sucesso', produto: novoProduto});
+    
+}); 
+
